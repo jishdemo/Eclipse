a9f0322331843b7f703798ae373d94a65b442642
Haykan <hajkan@mail.ru>
2017-09-10 17:46:19 +0300

    init project 10.9

---
+++ workspace/Git/src/gitTutorial/GitClass.java
@@ -0,0 +1,9 @@
+package gitTutorial;
+
+public class GitClass {
+	int data[]={4,5,6,7,9};
+
+	public void display(){
+		System.out.println("we are learning GIT");
+	}
+}
---
+++ workspace/Git/src/gitTutorial/GitDemo.java
@@ -0,0 +1,16 @@
+package gitTutorial;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		GitClass g1=new GitClass();
+		g1.display();
+		studyGit();
+	}
+		private static void studyGit(){
+			System.out.println("this is the first tutorial of GIT!!");
+			System.out.println("It is a video tutorial!!");
+		}
+	}
+
+
